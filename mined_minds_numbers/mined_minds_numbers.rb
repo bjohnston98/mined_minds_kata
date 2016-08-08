23e04a177f91ff23949fb13a4fa66d15fa24f34c
Brian Johnston <b_johnston98@yahoo.com>
2016-08-08 14:50:35 -0400

    Finally good code..

--- mined_minds_numbers/mined_minds_numbers.rb
+++ mined_minds_numbers/mined_minds_numbers.rb
@@ -1,15 +1,14 @@
 #mined_minds_numbers
 
 def mined_minds(number)
-	if number == 3
-		"mined"
-	elsif number == 5
-		"minds"
-	elsif number == 15
+	if number % 15 == 0
 			"minedminds"
+	elsif number % 3 == 0
+		"mined"
+	elsif number % 5 == 0
+		"minds"
 	else 
 		number
 	end
-
 end
 
--- mined_minds_test/mined_minds_test.rb
+++ mined_minds_test/mined_minds_test.rb
@@ -23,4 +23,20 @@ class Test_mined_minds < Minitest::Test
 	def test_3_and_5_returns_minedminds
 		assert_equal("minedminds", mined_minds(15))
 	end
+
+	def test_98_returns_98
+		assert_equal(98, mined_minds(98))
+	end
+
+	def test_30_returns_minedminds
+		assert_equal("minedminds", mined_minds(30))
+	end
+
+	def test_12_returns_mined
+		assert_equal("mined", mined_minds(12))
+	end
+
+	def test_10_returns_minds
+		assert_equal("minds", mined_minds(10))
+	end
 end
